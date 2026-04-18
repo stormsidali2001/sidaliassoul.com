0549339cb804d2f27b04037daa3ca5f1bc8c2dcf
Sidali Assoul <assoulsidali@gmail.com>
2026-04-18 17:57:41 +0100

    Added content attribute to the rss feed. Dev.to expect markdown there

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,23 +1,25 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-	const posts = (await getCollection('blog'))
-		.filter((p) => p.data.published !== false)
-		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+  const posts = (await getCollection("blog"))
+    .filter((p) => p.data.published !== false)
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		customData: `<language>en-us</language>`,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.description,
-			pubDate: post.data.pubDate,
-			link: `/blog/${post.id}/`,
-			categories: post.data.tags ?? [],
-		})),
-	});
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`,
+      categories: post.data.tags ?? [],
+
+      content: post.body,
+    })),
+  });
 }
